d5e7c0d168851ae66546443b43fbcbf3adf7bdc0
Max Han <mwellh84@gmail.com>
2020-03-02 16:57:43 -0500

    add products and user add to cart routes

--- server/api/users.js
+++ server/api/users.js
@@ -1,6 +1,6 @@
-const router = require('express').Router()
-const {User} = require('../db/models')
-module.exports = router
+const router = require('express').Router();
+const {User, Cart} = require('../db/models');
+module.exports = router;
 
 router.get('/', async (req, res, next) => {
   try {
@@ -9,9 +9,41 @@ router.get('/', async (req, res, next) => {
       // users' passwords are encrypted, it won't help if we just
       // send everything to anyone who asks!
       attributes: ['id', 'email']
-    })
-    res.json(users)
+    });
+    res.json(users);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
+
+router.get('/:userId', async (req, res, next) => {
+  const userId = req.params.userId;
+  try {
+    const user = await User.findByPk(userId);
+    const cartItems = await Cart.findAll({
+      where: {
+        userId: userId
+      }
+    });
+    res.json({...user, cart: cartItems});
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Add or remove item from cart
+router.post('/:userId', async (req, res, next) => {
+  try {
+    const item = await Cart.findOrCreate({
+      where: {
+        userId: req.params.userId,
+        itemId: req.body.itemId
+      }
+    });
+    item.quantity = req.body.quantity;
+    await item.update();
+    res.json(item);
+  } catch (err) {
+    next(err);
+  }
+});
