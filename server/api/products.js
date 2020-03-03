e6f95ecaea4f906eee02344af02bcb15c9c9c588
Nes Martinez <nes.alcides@gmail.com>
2020-03-03 15:57:44 -0500

    Added Create & Delete Product Routes

--- server/api/products.js
+++ server/api/products.js
@@ -37,4 +37,34 @@ router.post('/:productId', async (req, res, next) => {
   }
 });
 
+router.delete('/:productId', async (req, res, next) => {
+  try {
+    if (!req.user.isAdmin) {
+      const adminErr = new Error('Restricted');
+      adminErr.status = 405;
+      return next(adminErr);
+    }
+    const product = await Product.findByPk(req.params.productId);
+    if (!product) return res.sendStatus(404);
+    await product.destroy();
+    res.sendStatus(204);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post('/', async (req, res, next) => {
+  try {
+    if (!req.user.isAdmin) {
+      const adminErr = new Error('Restricted');
+      adminErr.status = 405;
+      return next(adminErr);
+    }
+    const product = await Product.create(req.body);
+    res.send(product);
+  } catch (err) {
+    next(err);
+  }
+});
+
 // post / to create a new product
