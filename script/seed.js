7e4b9398674c5a472503649fdbf876f73a877af0
Harry <habs@sdf.org>
2020-03-03 16:56:19 -0500

    add more seed data

--- script/seed.js
+++ script/seed.js
@@ -21,17 +21,56 @@ async function seed() {
       isAdmin: true,
       firstName: 'Murphy',
       lastName: 'Clark'
+    }),
+    User.create({
+      email: 'slothy@mcslothertonfamilyemailsite.net',
+      password: 'bamb00',
+      isAdmin: true,
+      firstName: 'Slothy',
+      lastName: 'McSlotherton'
     })
   ]);
 
   const products = await Promise.all([
     Product.create({
       name: 'Sloth Bed',
-      imageUrl: '/img/bed.jpg'
+      imageUrl: '/img/bed.jpg',
+      description:
+        'Only the finest Sloth Beds at Grace Slothpper. All beds are sloth-tested and extra comfortable!',
+      price: 14.99,
+      quantity: 10000
     }),
     Product.create({
       name: 'Sloth Mask',
-      imageUrl: '/img/mask.jpg'
+      imageUrl: '/img/mask.jpg',
+      description:
+        'With a frightening Corona Virus outbreak on the horizon, your sloths will need to be protected too! Get this Sloth Corona Virus Mask to keep your sloth free from harm. This product is sold without any warranty.',
+      price: 149.99,
+      quantity: 197
+    }),
+    Product.create({
+      name: 'Coronal Virus COVID-19 Vaccine',
+      imageUrl: '/img/vaccine.jpg',
+      description:
+        'Protect yourself and your sloth from the harmful Corona Virus with this affordable and exclusive patented vaccine! Our proprietary techology is the best at blocking the corona virus with counter virals that embed directly into the skin for maximum coverage. We do NOT accept any insurance!!!!',
+      price: 9999.99,
+      quantity: 11
+    }),
+    Product.create({
+      name: 'Corona Blocker 3000',
+      imageUrl: '/img/coronablocker3000.jpg',
+      description:
+        'Receive maximum protection from this high quality Corona Blocker mask! Manufactured in Wuhan, China, every mask has been human-tested in factory for Quality Assurance. 50% off for a limited time! (NOTE: for international orders, product will arrive in discreet unmarked packaging)',
+      price: 214.99,
+      quantity: 385
+    }),
+    Product.create({
+      name: 'Sloth Leaf',
+      imageUrl: '/img/leaf.jpg',
+      description:
+        'Please buy this delcious Leaf for your sloth friend! It is green and picked fresh from a local home grown tree. Makes for a meal that will have you saying, "Bon Appetit!!"',
+      price: 4.0,
+      quantity: 82
     })
   ]);
 
@@ -39,12 +78,22 @@ async function seed() {
     Cart.create({
       userId: users[0].id,
       productId: products[0].id,
-      quantity: 1
+      quantity: 6
+    }),
+    Cart.create({
+      userId: users[0].id,
+      productId: products[1].id,
+      quantity: 8
     }),
     Cart.create({
       userId: users[1].id,
-      productId: products[1].id,
+      productId: products[2].id,
       quantity: 2
+    }),
+    Cart.create({
+      userId: users[1].id,
+      productId: products[3].id,
+      quantity: 11
     })
   ]);
 
