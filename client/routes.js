9153834e91c550906e8a6300fab9522f99d955a1
Harry <habs@sdf.org>
2020-03-03 13:23:09 -0500

    add dispatches + routing for single products

--- client/routes.js
+++ client/routes.js
@@ -21,7 +21,7 @@ class Routes extends Component {
         {/* Routes placed here are available to all visitors */}
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
-        <Route path="/products" component={Products} />
+        <Route exact path="/products" component={Products} />
         <Route path="/products/:id" component={SingleProduct} />
         {isLoggedIn && (
           <Switch>
