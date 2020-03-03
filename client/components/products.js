69fae005b5166e3552fa9f0d060fa434922184d1
Harry <habs@sdf.org>
2020-03-03 11:27:01 -0500

    fix react page to add componentDidMount

--- client/components/products.js
+++ client/components/products.js
@@ -1,28 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
+import {getProducts} from '../store';
 
 /**
  * COMPONENT
  */
-export const Products = props => {
-  const {products} = props;
-
-  return (
-    <div>
-      {products.map(p => (
-        <div>
-          <h3>
-            {p.name} &mdash; ${p.price}
-          </h3>
-          <img src={p.imageUrl} />
-          <p>{p.description}</p>
-          <p>Qty: {p.quantity}</p>
-        </div>
-      ))}
-    </div>
-  );
-};
+class Products extends React.Component {
+  componentDidMount() {
+    getProducts();
+  }
+  render() {
+    const {products} = this.props;
+    return (
+      <div>
+        {products.map(p => (
+          <div>
+            <h3>
+              {p.name} &mdash; ${p.price}
+            </h3>
+            <img src={p.imageUrl} />
+            <p>{p.description}</p>
+            <p>Qty: {p.quantity}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
 
 /**
  * CONTAINER
