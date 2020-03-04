e7c0b403802698a4fac06ca43e8be2de31a4da51
Mercedes <mercedesm@gmail.com>
2020-03-04 16:21:18 -0500

    Added DeletedLineItem to store

--- client/store/order.js
+++ client/store/order.js
@@ -7,20 +7,33 @@ import history from '../history';
 const GOT_CART = 'GOT_CART';
 const ADDED_TO_CART = 'ADDED_TO_CART';
 const CHECKED_OUT = 'CHECKED_OUT';
+const DELETED_LINE_ITEM = 'DELETED_LINE_ITEM';
 
 /**
  * ACTION CREATORS
  */
 const gotCart = cart => ({type: GOT_CART, cart});
+
 const addedToCart = (userId, lineItem) => ({
   type: ADDED_TO_CART,
   userId,
   lineItem
 });
+
+const deletedLineItem = (userId, productId) => ({
+  type: DELETED_LINE_ITEM,
+  userId,
+  productId
+});
+
 const checkedOut = userId => ({
   type: CHECKED_OUT,
   userId
 });
+
+/**
+ * THUNK
+ */
 
 export const getCart = userId => async dispatch => {
   try {
@@ -52,6 +65,17 @@ export const checkOut = userId => async dispatch => {
   }
 };
 
+export const deleteLineItem = (userId, productId) => async dispatch => {
+  try {
+    const {data} = await axios.delete(
+      `/api/orders/cart/${userId}/${productId}`
+    );
+    dispatch(deletedLineItem(userId, productId));
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 /**
  * REDUCER
  */
@@ -61,6 +85,8 @@ export default function(state = [], action) {
       return action.cart;
     case CHECKED_OUT:
       return [];
+    case DELETED_LINE_ITEM:
+      return state.filter(item => item.id !== action.id);
     default:
       return state;
   }
