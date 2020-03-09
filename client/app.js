9e0765f461f8937baea349e9c0fc332384ee5c14
Mercedes <mercedesm@gmail.com>
2020-03-09 16:09:55 -0400

    Feature: Footer

--- client/app.js
+++ client/app.js
@@ -1,15 +1,16 @@
-import React from 'react'
+import React from 'react';
 
-import {Navbar} from './components'
-import Routes from './routes'
+import {Navbar, Footer} from './components';
+import Routes from './routes';
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Routes />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- client/components/index.js
+++ client/components/index.js
@@ -4,6 +4,7 @@
  * having to figure out which file they belong to!
  */
 export {default as Navbar} from './navbar';
+export {default as Footer} from './footer';
 export {default as UserCart} from './user-cart';
 export {default as Home} from './home';
 export {Login, Signup} from './auth-form';
